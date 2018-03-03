3b1042a30a64d16f6fc2c316dd795518c8d67dc6
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2018-03-03 13:20:48 +0900

    feat(all): update

---
+++ src/components/Demo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="demo">
+    <p>{{title}}</p>
+    <router-link to="/">back</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'test'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .demo {
+    color: #ffffff;
+  }
+</style>
